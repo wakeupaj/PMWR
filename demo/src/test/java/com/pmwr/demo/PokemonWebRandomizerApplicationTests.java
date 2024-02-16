d3ef9d18d1fce4429f669775d70fa1bd5f85a942
aj <aj@glostars.net>
2024-02-15 21:16:36 -0500

    Initialized Springboot.

---
+++ demo/src/test/java/com/pmwr/demo/PokemonWebRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmwr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonWebRandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
