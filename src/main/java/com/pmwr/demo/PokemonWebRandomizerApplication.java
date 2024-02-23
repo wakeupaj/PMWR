6fca1556a125b43a2a63a46c9dbed25fdf67b653
ajensiva <ajensiva@hotmail.com>
2024-02-22 21:28:08 -0500

    basic setup and can upload a file now (calm luh commit)

--- src/main/java/com/pmwr/demo/PokemonWebRandomizerApplication.java
+++ src/main/java/com/pmwr/demo/PokemonWebRandomizerApplication.java
@@ -2,12 +2,18 @@ package com.pmwr.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class PokemonWebRandomizerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokemonWebRandomizerApplication.class, args);
 	}
-
+	@GetMapping("/")
+    public String displayUploadForm() {
+        return "upload"; // This refers to upload.html in the templates directory
+    }
 }
